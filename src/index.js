ad8e7a1c326a170dbbe3c7d9e60d62f7280a4890
Bilinear <Bilinear@succ.com>
2020-04-14 19:17:16 +0800

    Export HashParserHOC API

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import GUI from './containers/gui.jsx';
 import AppStateHOC from './lib/app-state-hoc.jsx';
+import HashParserHOC from './lib/hash-parser-hoc.jsx';
 import GuiReducer, {guiInitialState, guiMiddleware, initEmbedded, initFullScreen, initPlayer} from './reducers/gui';
 import LocalesReducer, {localesInitialState, initLocale} from './reducers/locales';
 import {ScratchPaintReducer} from 'scratch-paint';
@@ -16,6 +17,7 @@ const guiReducers = {
 export {
     GUI as default,
     AppStateHOC,
+    HashParserHOC,
     setAppElement,
     guiReducers,
     guiInitialState,

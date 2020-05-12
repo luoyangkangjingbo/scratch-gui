6dd1cd80599ab08bc09de88426f69b086658bf43
Bilinear <Bilinear@succ.com>
2020-05-12 15:22:39 +0800

    scale for mini-screen, for example pad

--- src/playground/index.jsx
+++ src/playground/index.jsx
@@ -17,9 +17,30 @@ import styles from './index.css';
 // Register "base" page view
 analytics.pageview('/');
 
+function isPC() {
+    var clientInfo = navigator.userAgent
+    // Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.122 Safari/537.36
+    if (clientInfo.indexOf('(Windows') !== -1) {
+        return true
+    } else {
+        return false
+    }
+}
+
 const appTarget = document.createElement('div');
 appTarget.className = styles.app;
 document.body.appendChild(appTarget);
+if ((!isPC())  && (document.body.clientWidth < 1024)) {
+    appTarget.style.width = '1024px';
+    appTarget.style.height = '640px';
+    var widthScale  = document.body.clientWidth / 1024
+    var heightScale = document.body.clientHeight / 640
+    appTarget.style.transform = `scale(${widthScale}, ${heightScale})`
+    appTarget.style.webkitTransform = `scale(${widthScale}, ${heightScale})`
+} else {
+    appTarget.style.minWidth  = '1024px'
+    appTarget.style.minHeight = '640px'
+}
 
 if (supportedBrowser()) {
     // require needed here to avoid importing unsupported browser-crashing code
